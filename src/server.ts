6d02ad0a8d7c97470d7822b99a3aa98a3b10dd8e
joshuawprich <70492614+joshuawprich@users.noreply.github.com>
2022-05-05 08:41:10 +1200

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,37 @@ import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as logger from "koa-logger";
 import * as bodyParser from "koa-bodyparser";
+import HttpStatus from "http-status-codes";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Koa = new Koa();
 const router = new Router();
 
-router.get("/", async (ctx: Koa.Context) => {
-  const name = ctx.query.name || "stranger";
-  ctx.body = {
-    message: `Hello, ${name}!`
-  };
+//Generic error handling
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status =
+      err.statusCode || err.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    err.status = ctx.status;
+    ctx.body = { err };
+    ctx.app.emit("error", err, ctx);
+  }
 });
+
+// Initial route
+app.use(async (ctx: Koa.Context) => {
+  ctx.body = "Hello world";
+});
+
+// router.get("/", async (ctx: Koa.Context) => {
+//   const name = ctx.query.name || "stranger";
+//   ctx.body = {
+//     message: `Hello, ${name}!`
+//   };
+// });
 
 app
   .use(logger())
