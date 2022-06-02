e2f5dcb29405d26de7efb7b18a516052a4061f4f
joshuawprich <70492614+joshuawprich@users.noreply.github.com>
2022-06-02 04:50:53 +0000

    Removed dist folder, not sure where that came from.

--- dist/routes/index.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const routerOpts = {};
-const router = new Router(routerOpts);
-router.get("/", async (ctx) => {
-    ctx.body = "Index Page";
-});
-exports.default = router;
