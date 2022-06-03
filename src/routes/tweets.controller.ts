4744a08aeab87b93abb660f47fa095db0482d587
joshuawprich <70492614+joshuawprich@users.noreply.github.com>
2022-06-03 00:07:02 +0000

    Added functionality to accept more that one tweet.
    Added comments.

--- src/routes/tweets.controller.ts
+++ src/routes/tweets.controller.ts
@@ -1,25 +1,36 @@
 import * as Koa from "koa";
 import * as Router from "koa-router";
+import *  as Http from "http-status-codes";
 import axios from "axios";
-import HttpStatus from "http-status-codes";
 
 require("dotenv").config();
 
-const token: string = process.env.TWITTER_BEARER_TOKEN;
+const token:string = process.env.TWITTER_BEARER_TOKEN;
 
 const routerOpts: Router.IRouterOptions = {
   prefix: "/tweets"
 };
 
-const baseURL: string = "https://api.twitter.com/2/tweets/";
+const baseURL:string = "https://api.twitter.com/2/tweets";
 
-const router: Router = new Router(routerOpts);
+const router:Router = new Router(routerOpts);
 
-async function getTweet(tweetID: string | string[]) {
-  let url: string = baseURL + tweetID;
+// Funtion for retrieving the tweet object from
+// the Twitter Api given the Tweet ID/s
+async function getTweet(tweetID:string|string[]) {
 
-  console.log(tweetID)
+  let url:string;
 
+  // Check if the given ID string contains more than 1 ID
+  if(tweetID.includes(',')) {
+    url = baseURL.concat("?ids=")
+  } else {
+    url = baseURL.concat("/")
+  }
+
+  url += tweetID;
+
+  // Request config
   var config = {
     method: "get",
     url: url,
@@ -28,29 +39,35 @@ async function getTweet(tweetID: string | string[]) {
     }
   };
 
+  // Async request function
   return await axios(config)
     .then(function (response) {
+
       let data = JSON.stringify(response.data);
+
       if (response.data.errors) {
 
         let error = response.data.errors[0]
         return "Error: " + error.detail;
         
       } else {
-        return data;
+        let tweet:string = data
+        return tweet;
       }
     })
     .catch(function (error) {
-      console.log(error);
+      let errorMessage:string[] = error.response.data 
+      return errorMessage;
     });
 }
 
-//async function getRequest() {}
-
+// End point for retrieving the Tweets
 router.get("/", async (ctx: Koa.Context) => {
-  let response = await getTweet(ctx.query.tweetID);
 
-  console.log(response);
+  let query:string|string[] = ctx.query.ids
+
+  let response = await getTweet(query);
+
   ctx.body = response;
 });
 
